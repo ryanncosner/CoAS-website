8f0d361a860a20610891b02ce7bdc24a3157aaeb
ryanncosner <133050151+ryanncosner@users.noreply.github.com>
2025-08-20 16:46:39 -0400

    Update data.js

--- data.js
+++ data.js
@@ -3,8 +3,9 @@
 // EDIT THIS OBJECT TO CHANGE THE ACTIVITY FOR EVERYONE
 // ------------------------------------------------------------------------------
 const coasActivity = {
-  activity: "Fortnite", // The value from the <select> menu (e.g., "work", "study", "test")
-  startTime: "21:00", // Must be in 24-hour "HH:MM" format
-  imageUrl: "fortnite.webp" // Optional: A link to an image (either a full URL or a data URL)
+  activity: "Grounded", // The value from the <select> menu (e.g., "work", "study", "test")
+  startTime: "20:00", // Must be in 24-hour "HH:MM" format
+  imageUrl: "Grounded.jpg" // Optional: A link to an image (either a full URL or a data URL)
 };
+
 // ------------------------------------------------------------------------------
