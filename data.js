bcc5afcf80e99e431480637a51ded22f6dbd4d94
ryanncosner <133050151+ryanncosner@users.noreply.github.com>
2025-08-19 20:53:48 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+// data.js
+// ------------------------------------------------------------------------------
+// EDIT THIS OBJECT TO CHANGE THE ACTIVITY FOR EVERYONE
+// ------------------------------------------------------------------------------
+const coasActivity = {
+  activity: "Fortnite", // The value from the <select> menu (e.g., "work", "study", "test")
+  startTime: "21:00", // Must be in 24-hour "HH:MM" format
+  imageUrl: "fortnite.webp" // Optional: A link to an image (either a full URL or a data URL)
+};
+// ------------------------------------------------------------------------------
