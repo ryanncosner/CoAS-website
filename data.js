3cd70da259685315596754c2cb65a04de92ee16a
ryanncosner <133050151+ryanncosner@users.noreply.github.com>
2025-08-20 16:47:59 -0400

    Update data.js

--- data.js
+++ data.js
@@ -4,8 +4,9 @@
 // ------------------------------------------------------------------------------
 const coasActivity = {
   activity: "Grounded", // The value from the <select> menu (e.g., "work", "study", "test")
-  startTime: "20:00", // Must be in 24-hour "HH:MM" format
+  startTime: "20:30", // Must be in 24-hour "HH:MM" format
   imageUrl: "Grounded.jpg" // Optional: A link to an image (either a full URL or a data URL)
 };
 
 // ------------------------------------------------------------------------------
+
