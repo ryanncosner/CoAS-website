ad797cef28967bc24c65f18ca5a1bef05efee3a9
ryanncosner <133050151+ryanncosner@users.noreply.github.com>
2025-08-22 23:38:43 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -3,11 +3,8 @@
 // EDIT THIS OBJECT TO CHANGE THE ACTIVITY FOR EVERYONE
 // ------------------------------------------------------------------------------
 const coasActivity = {
-  activity: "Grounded", // The value from the <select> menu (e.g., "work", "study", "test")
-  startTime: "20:30", // Must be in 24-hour "HH:MM" format
-  imageUrl: "Grounded.jpg" // Optional: A link to an image (either a full URL or a data URL)
+  activity: "Dead By Deadlight", // The value from the <select> menu (e.g., "work", "study", "test")
+  startTime: "23:30", // Must be in 24-hour "HH:MM" format
+  imageUrl: "dead-by-daylight.webp" // Optional: A link to an image (either a full URL or a data URL)
 };
-
 // ------------------------------------------------------------------------------
-
-
